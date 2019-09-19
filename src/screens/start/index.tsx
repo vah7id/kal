45f944b99400397866317ef24a7e7471025fb908
Vahid Taghizadeh <vahid@brenger.nl>
2019-09-20 00:23:58 +0200

    arcade menu buttons with the sound added

--- src/screens/start/index.tsx
+++ src/screens/start/index.tsx
@@ -5,6 +5,25 @@ class Start extends Component<any, any> {
     componentDidMount() {
         const menu = document.querySelector('.screen--start ul') as HTMLElement;
         menu.style.maxWidth = window.screen.width + 'px';
+        this.buttonEventListeners();
+    }
+    buttonEventListeners() {
+        const buttons: any = document.querySelectorAll('.screen--start ul li button');
+        buttons.forEach((btn: HTMLElement) => {
+            btn.addEventListener('focus', (e: any) => {
+                this.blurAllMenuButtons();
+                e.currentTarget.classList.add('active');
+                const audio = document.getElementById("sound-select-btn") as HTMLAudioElement;
+                audio.play();
+            }, true);
+            buttons[0].focus();
+        });
+    }
+    blurAllMenuButtons() {
+        const buttons: any = document.querySelectorAll('.screen--start ul li button');
+        buttons.forEach((btn: HTMLElement) => {
+            btn.classList.remove('active');
+        });
     }
     render() {
         return (
@@ -12,7 +31,11 @@ class Start extends Component<any, any> {
                 <ul>
                     <li><button>single player</button></li>
                     <li><button>multi player</button></li>
+                    <li><button>settings</button></li>
+                    <li><button>how to play</button></li>
+                    <li><button>credits</button></li>
                 </ul>
+                <audio id={"sound-select-btn"} src={'https://opengameart.org/sites/default/files/audio_preview/mouseclick.wav.mp3'} ></audio>
             </div>
         );
     }

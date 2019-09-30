f2b4d4a4645d2d56c8207667809e672ff6b21471
Vahid Taghizadeh <vahid@brenger.nl>
2019-09-30 14:37:19 +0200

    Updates

---
+++ precache-manifest.d8dc076135dae5fe795867b52bccf806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be29d37b079695368cbca22c4af05c7",
+    "url": "/kal/index.html"
+  },
+  {
+    "revision": "7204db1a85fdf1ebffc6",
+    "url": "/kal/static/css/main.b08dbe0d.chunk.css"
+  },
+  {
+    "revision": "49ad82458c6b296af5f1",
+    "url": "/kal/static/js/2.c2caee85.chunk.js"
+  },
+  {
+    "revision": "7204db1a85fdf1ebffc6",
+    "url": "/kal/static/js/main.aeb64d06.chunk.js"
+  },
+  {
+    "revision": "4636e13a7b203dbe2473",
+    "url": "/kal/static/js/runtime~main.98893dc1.js"
+  },
+  {
+    "revision": "599c58a93cbdc44078fd51e4d606e7db",
+    "url": "/kal/static/media/stone.599c58a9.png"
+  }
+]);

e3ff52fa662a9d27540503ffb7c5d97156410631
Vahid Taghizadeh <vahid@brenger.nl>
2019-10-01 18:45:55 +0200

    reloading label added to kick off screen

--- src/screens/game/saga.ts
+++ src/screens/game/saga.ts
@@ -1,11 +1,19 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import { actions, types } from './reducer';
+import {LOCATION_CHANGE} from "connected-react-router";
 
 function* kickoff() {
     yield put(actions.playing());
 }
 
+function* reset(action: any) {
+    if(action.payload.action === 'POP' && action.payload.location.pathname === '/' && !action.payload.isFirstRendering) {
+        window.location.reload();
+    }
+}
+
 export function* gameSaga() {
+    yield takeLatest(LOCATION_CHANGE, reset);
     yield takeLatest(types.KICK_OFF, kickoff);
 }
 
--- src/screens/start/menu.tsx
+++ src/screens/start/menu.tsx
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 import Button from '../../assets/elements/Button';
 import '../../assets/styles/_start.scss';
 import { push } from 'connected-react-router';
-
-interface State {
-    status: string;
-}
+import {GAME_STATUS} from "../game/types";
 
 interface Props {
     navigate: any;
+    status: string;
 }
 
-class StartMenu extends Component<Props, State> {
+class StartMenu extends Component<Props, any> {
     componentDidMount() {
-        this.adjustMenuSize();
-        this.initialMenu();
+        if(this.props.status === GAME_STATUS.kickoff) {
+            this.adjustMenuSize();
+            this.initialMenu();
+        }
     }
     adjustMenuSize() {
         const menu = document.querySelector('.screen--start ul') as HTMLElement;
@@ -31,13 +31,17 @@ class StartMenu extends Component<Props, State> {
     render() {
         return (
             <>
-                <ul>
-                    <li><Button onPress={this.onClickMenu.bind(this)} id={'single-player'} type={'link'} title={'single player'} /></li>
-                    <li><Button onPress={this.onClickMenu.bind(this)} id={'multi-player'} type={'link'} title={'multi player'} /></li>
-                    <li><Button onPress={this.onClickMenu.bind(this)} id={'settings'} type={'link'} title={'settings'} /></li>
-                    <li><Button onPress={this.onClickMenu.bind(this)} id={'guidance'} type={'link'} title={'how to play'} /></li>
-                    <li><Button onPress={this.onClickMenu.bind(this)} id={'credits'} type={'link'} title={'credits'} /></li>
-                </ul>
+                {
+                    this.props.status !== GAME_STATUS.playing ?
+                    <ul>
+                        <li><Button onPress={this.onClickMenu.bind(this)} id={'single-player'} type={'link'} title={'single player'} /></li>
+                        <li><Button onPress={this.onClickMenu.bind(this)} id={'multi-player'} type={'link'} title={'multi player'} /></li>
+                        <li><Button onPress={this.onClickMenu.bind(this)} id={'settings'} type={'link'} title={'settings'} /></li>
+                        <li><Button onPress={this.onClickMenu.bind(this)} id={'guidance'} type={'link'} title={'how to play'} /></li>
+                        <li><Button onPress={this.onClickMenu.bind(this)} id={'credits'} type={'link'} title={'credits'} /></li>
+                    </ul> :
+                    <h2>RELOADING...</h2>
+                }
             </>
         )
     }
